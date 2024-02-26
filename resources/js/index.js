ea9e185dbb3fcf1aa01e932607b7cb2a626a2397
Meet Jani <meet.j@dc.cloudoffis.com.au>
2024-02-26 11:57:39 +0530

    Responsive Html Template (First Commit)

---
+++ resources/js/index.js
@@ -0,0 +1,9 @@
+function muFunction(){
+    var x = document.getElementById("t-menu");
+    
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
